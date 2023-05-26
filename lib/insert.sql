1f5ef4ab3ef665a0b7aac8d598a670ceccb13391
Historia Montague <historia.montague@outlook.com>
2023-05-26 09:24:36 -0600

    Finished Lab

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears 
+    (name, age, sex, color, temperament, alive) 
+VALUES 
+    ("Mr. Chocolate", 3, "F", "Brown", "angy", 1),
+    ("Rowdy", 3, "F", "Brown", "angy", 1),
+    ("Tabitha", 3, "F", "Brown", "angy", 1),
+    ("Sergeant Brown", 3, "F", "Brown", "angy", 1),
+    ("Melissa", 3, "F", "Brown", "angy", 1),
+    ("Grinch", 3, "F", "Brown", "angy", 1),
+    ("Wendy", 3, "F", "Brown", "angy", 1),
+    (NULL, NULL, NULL, NULL, NULL, NULL)
